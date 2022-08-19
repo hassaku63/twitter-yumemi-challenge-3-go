0582ce12070cd9f881c9736f768812907cb86233
hassaku63 <hassaku63@gmail.com>
2022-08-19 21:12:25 +0900

    出題

--- main.go
+++ main.go
@@ -1,3 +1,11 @@
 package main
 
-func main() {}
+import "fmt"
+
+func main() {
+	array1 := []int{1, 2, 3, 4, 5, 6}
+	array2 := array1 /* TODO */
+
+	fmt.Println(array2)
+	// expect: [1 1 1 2 2 3 3 3 4 4 5 5 5 6 6]
+}
