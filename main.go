970f9c23fae5e6e5a82f3dc68dad53c3d5a81369
hassaku63 <hassaku63@gmail.com>
2022-08-19 21:31:21 +0900

    回答その1
    ワンライナーは無理そうだと思ったのでそれっぽい関数を追加することで
    main では式 1 つに収まるようにしてみた

--- main.go
+++ main.go
@@ -4,8 +4,23 @@ import "fmt"
 
 func main() {
 	array1 := []int{1, 2, 3, 4, 5, 6}
-	array2 := array1 /* TODO */
+	array2 := FlatMap(&array1, func(arr *[]int, v int, i int) []int {
+		if v%2 == 0 {
+			return []int{v, v}
+		} else {
+			return []int{v, v, v}
+		}
+	})
 
 	fmt.Println(array2)
 	// expect: [1 1 1 2 2 3 3 3 4 4 5 5 5 6 6]
+}
+
+func FlatMap(array *[]int, fn func(arr *[]int, value int, index int) []int) []int {
+	result := make([]int, 0)
+	for i, v := range *array {
+		ret := fn(array, v, i)
+		result = append(result, ret...)
+	}
+	return result
 }
